0dbbad82ced69d1d2f445451139e49150e5c31a8
Abdall-Moha <wizzcabdala9@gmail.com>
2026-07-07 10:39:47 -0700

    fix: update import statement for authSlice to match folder case sensitivity

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import {login} from "../redux/authSlice.js";
+import {login} from "../Redux/authSlice.js";
 import { useNavigate } from "react-router-dom";
 import {
   User,
